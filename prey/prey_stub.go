f807bb0a99670d7750bf64d026ca3c9e66dc03c4
Joaliste <jorgeignacio.aliste@mercadolibre.cl>
2024-02-12 12:16:06 -0300

    feat: exercise 2, implementing prey stub

---
+++ prey/prey_stub.go
@@ -0,0 +1,29 @@
+package prey
+
+import (
+	"testdoubles/positioner"
+)
+
+// NewPreyStub creates a new prey stub
+func NewPreyStub() *PreyStub {
+	return &PreyStub{}
+}
+
+// PreyStub is an implementation of the Prey interface
+type PreyStub struct {
+	// GetSpeedFunc is a replacement of the GetSpeed function
+	GetSpeedFunc func() (speed float64)
+	// GetPositionFunc is a replacement of the GetPosition function
+	GetPositionFunc func() (position *positioner.Position)
+}
+
+// GetSpeed returns the speed of the prey
+func (ps *PreyStub) GetSpeed() (speed float64) {
+	speed = ps.GetSpeed()
+	return
+}
+
+func (ps *PreyStub) GetPosition() (position *positioner.Position) {
+	position = ps.GetPosition()
+	return
+}
