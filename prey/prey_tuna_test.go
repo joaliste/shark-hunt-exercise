9cd011088f10ebfdc959a9d1f481a4d418b2f188
Joaliste <jorgeignacio.aliste@mercadolibre.cl>
2024-02-13 12:52:32 -0300

    feat: tests for prey_tuna GetPosition method

--- prey/prey_tuna_test.go
+++ prey/prey_tuna_test.go
@@ -2,6 +2,7 @@ package prey_test
 
 import (
 	"github.com/stretchr/testify/require"
+	"testdoubles/positioner"
 	"testdoubles/prey"
 	"testing"
 )
@@ -26,5 +27,27 @@ func TestTuna_GetSpeed(t *testing.T) {
 		speed := tuna.GetSpeed()
 		// assert
 		require.Equal(t, expectedSpeed, speed)
+	})
+}
+
+func TestTuna_GetPosition(t *testing.T) {
+	t.Run("position is nil", func(t *testing.T) {
+		// arrange
+		tuna := prey.NewTuna(0.0, nil)
+		// act
+		position := tuna.GetPosition()
+		// assert
+		require.Nil(t, position)
+	})
+
+	t.Run("position is default (0,0,0)", func(t *testing.T) {
+		// arrange
+		expectedPosition := &positioner.Position{X: 0.0, Y: 0.0, Z: 0.0}
+		tuna := prey.NewTuna(0.0, expectedPosition)
+		// act
+		position := tuna.GetPosition()
+		// assert
+		require.Equal(t, expectedPosition, position)
+		require.NotNil(t, position)
 	})
 }
